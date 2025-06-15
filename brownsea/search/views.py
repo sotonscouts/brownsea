913d7282ca0be6382a153c238d9d29a67d01370a
Dan Trickey <git@trickey.io>
2025-06-15 11:15:33 +0100

    feat: search page styling

--- brownsea/search/views.py
+++ brownsea/search/views.py
@@ -37,7 +37,7 @@ def search(request):
 
     return TemplateResponse(
         request,
-        "search/search.html",
+        "pages/search/search.html",
         {
             "search_query": search_query,
             "search_results": search_results,
