8b3e1169723aa7cf7997c0088f6e088c51c71af2
Dan Trickey <git@trickey.io>
2025-06-22 14:23:51 +0100

    fix: unavailable sentry options

--- brownsea/core/settings/base.py
+++ brownsea/core/settings/base.py
@@ -262,16 +262,12 @@ if SENTRY_DSN:
     SENTRY_ENVIRONMENT = env("SENTRY_ENVIRONMENT")
     SENTRY_TRACES_SAMPLE_RATE = env("SENTRY_TRACES_SAMPLE_RATE", default=0.1)
     SENTRY_SEND_DEFAULT_PII = env("SENTRY_SEND_DEFAULT_PII", default=True)
-    SENTRY_AUTO_SESSION_TRACKING = env("SENTRY_AUTO_SESSION_TRACKING", default=True)
-    SENTRY_AUTO_TRANSACTION_TRACKING = env("SENTRY_AUTO_TRANSACTION_TRACKING", default=True)
 
     sentry_sdk.init(
         dsn=SENTRY_DSN,
         environment=SENTRY_ENVIRONMENT,
         traces_sample_rate=SENTRY_TRACES_SAMPLE_RATE,
         send_default_pii=SENTRY_SEND_DEFAULT_PII,
-        auto_session_tracking=SENTRY_AUTO_SESSION_TRACKING,
-        auto_transaction_tracking=SENTRY_AUTO_TRANSACTION_TRACKING,
     )
 
 # App Specific Settings
